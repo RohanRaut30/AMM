df5b033d632495f0e52d0c50f544e01b0261f16f
Rohan Raut <Rohanraut2300@gmail.com>
2023-06-23 14:55:03 +0530

    added language support!!!!

--- app/src/main/java/com/example/amm/MainActivity.java
+++ app/src/main/java/com/example/amm/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.amm;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,15 +21,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.amm.databinding.ActivityMainBinding;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    Button btnC;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        loadLocale();
+//        btnC = findViewById(R.id.btnChangeLanguage);
+//
+//        btnC.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                changLanguage();
+//            }
+//        });
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -35,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                changLanguage();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -48,6 +67,50 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void changLanguage() {
+        final String languages[] = {"English","Marathi","Hindi"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+        mBuilder.setTitle("Choose Language");
+        mBuilder.setSingleChoiceItems(languages, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which==0) {
+                    setLocal("");
+                    recreate();
+                }
+                if(which==1) {
+                    setLocal("mr");
+                    recreate();
+                }
+                if(which==2) {
+                    setLocal("hi");
+                    recreate();
+                }
+            }
+        });
+        mBuilder.create();
+        mBuilder.show();
+    }
+
+    private void setLocal(String language){
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = new Configuration();
+        configuration.locale = locale;
+        getBaseContext().getResources().updateConfiguration(configuration, getBaseContext()
+                .getResources().getDisplayMetrics());
+
+        SharedPreferences.Editor editor = getSharedPreferences("Settings",MODE_PRIVATE).edit();
+        editor.putString("app_lang",language);
+        editor.apply();
+    }
+    private  void loadLocale(){
+        SharedPreferences preferences = getSharedPreferences("Settings",MODE_PRIVATE);
+        String language = preferences.getString("app_lang", "");
+        setLocal(language);
     }
 
     @Override
