67e40b3947121b5c45273a34a06e9e40a8693bf9
Omkar Suryawanshi <suryawanshiomkar2019@gmail.com>
2023-06-29 21:38:52 +0530

    29/6/2023 Navigation drawer updated,contact us page updated with firebase, we are eco friendly updated, Admin Login done firebase

--- app/src/main/java/com/example/amm/Fragment/AdminLogin.java
+++ app/src/main/java/com/example/amm/Fragment/AdminLogin.java
@@ -7,60 +7,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.amm.R;
+import com.example.amm.ui.home.HomeFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminLogin#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdminLogin extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AdminLogin() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminLogin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminLogin newInstance(String param1, String param2) {
-        AdminLogin fragment = new AdminLogin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_login, container, false);
+
+
+        // Find views
+        usernameEditText = view.findViewById(R.id.usernameEditText);
+        passwordEditText = view.findViewById(R.id.passwordEditText);
+        loginButton = view.findViewById(R.id.loginButton);
+
+        // Set click listener for login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve entered username and password
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                // Validate credentials
+                if (isValidCredentials(username, password)) {
+                    // Login successful, perform desired action (e.g., navigate to admin dashboard)
+                    // Navigate to the AboutUsFragment
+                    NavHostFragment.findNavController(AdminLogin.this).navigate(R.id.action_admin_to_adminHome);
+                    Toast.makeText(requireContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                    // TODO: Perform action after successful login
+                } else {
+                    // Invalid credentials, show error message
+                    Toast.makeText(requireContext(), "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        return view;
+    }
+
+
+    private boolean isValidCredentials(String username, String password) {
+        // Perform your own validation logic here (e.g., check against a database)
+        // For demonstration purposes, consider hardcoded valid credentials
+        String validUsername = "Admin";
+        String validPassword = "Admin@214";
+
+        return username.equals(validUsername) && password.equals(validPassword);
     }
 }
