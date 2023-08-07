f986d24e869a61a08ef9ec25bc58e94fb53530a4
Rohan Raut <Rohanraut2300@gmail.com>
2023-08-07 11:59:23 +0530

    fixed lang. support..

--- app/src/main/java/com/example/amm/ShareFragment.java
+++ app/src/main/java/com/example/amm/ShareFragment.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 
 public class ShareFragment extends Fragment {
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
